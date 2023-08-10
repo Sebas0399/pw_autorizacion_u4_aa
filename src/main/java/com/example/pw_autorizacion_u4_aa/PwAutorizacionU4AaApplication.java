b4f4a7fe4ccf07e346981f00afdf3ae633db2c88
Sebas0399 <antony.aguay@gmail.com>
2023-08-09 19:45:58 -0500

    first commit

---
+++ src/main/java/com/example/pw_autorizacion_u4_aa/PwAutorizacionU4AaApplication.java
@@ -0,0 +1,13 @@
+package com.example.pw_autorizacion_u4_aa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwAutorizacionU4AaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PwAutorizacionU4AaApplication.class, args);
+	}
+
+}
